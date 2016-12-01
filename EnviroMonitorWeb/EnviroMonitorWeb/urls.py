1b87b45062fde7ed5a4ddba10950572b536a433c
grillazz <the@grillazz.com>
2016-12-01 16:14:23 +0100

    swagger endpoint added

--- EnviroMonitorWeb/EnviroMonitorWeb/urls.py
+++ EnviroMonitorWeb/EnviroMonitorWeb/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from docs.views import schema_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api.urls')),
+    url(r'^docs/',  schema_view),
 ]
