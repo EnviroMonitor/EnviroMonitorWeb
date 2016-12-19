1df0dfe7f3f354ac1b70d40e3ac07fdf5eef7e93
grillazz <the@grillazz.com>
2016-12-19 13:52:09 +0100

    add custom fully resposive layout for browsable rest api  - update settngs.py

--- EnviroMonitorWeb/EnviroMonitorWeb/settings.py
+++ EnviroMonitorWeb/EnviroMonitorWeb/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'EnviroMonitorWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # User settings
 
