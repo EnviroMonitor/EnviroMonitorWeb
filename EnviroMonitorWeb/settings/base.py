db6b085642d36334e7c6628b95bfd176e0525495
grillazz <the@grillazz.com>
2017-01-24 15:31:54 +0100

    REST API basic auth endpoint added

--- EnviroMonitorWeb/settings/base.py
+++ EnviroMonitorWeb/settings/base.py
@@ -139,9 +139,6 @@ REST_FRAMEWORK = {
         'rest_framework_xml.renderers.XMLRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
