c0ea942301cc72bddf4fe0dad779f2b5bdb23d25
grillazz <the@grillazz.com>
2017-01-26 14:09:11 +0100

    rename user_factory to user

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -11,22 +11,22 @@ class ProjectApiTests(APITestCase):
     def setUp(self):
         self.api_data = factory_build(dict, FACTORY_CLASS=ProjectFactoryForTesting)
         self.api_url = reverse('project-list')
-        self.user_factory = UserFactory()
+        self.user = UserFactory()
 
     def test_project_create(self):
-        self.client.login(username=self.user_factory.username, password=user_password)
+        self.client.login(username=self.user.username, password=user_password)
         api_response = self.client.post(self.api_url, self.api_data, format='json')
         self.assertEqual(api_response.status_code, HTTP_201_CREATED)
         self.assertEqual(Project.objects.count(), 1)
         self.assertEqual(Project.objects.get().name, self.api_data['name'])
 
     def test_project_list(self):
-        self.client.login(username=self.user_factory.username, password=user_password)
+        self.client.login(username=self.user.username, password=user_password)
         api_response = self.client.get(self.api_url)
         self.assertEqual(api_response.status_code, HTTP_200_OK)
 
     def test_message_get(self):
-        self.client.login(username=self.user_factory.username, password=user_password)
+        self.client.login(username=self.user.username, password=user_password)
         api_response = self.client.post(self.api_url, self.api_data, format='json')
         self.assertEqual(api_response.status_code, HTTP_201_CREATED)
         api_response = self.client.get('/api/v1/project/1/')
