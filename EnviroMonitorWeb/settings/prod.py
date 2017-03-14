1d1339a00c6b29dcb06cac1fdafebde5858da5b9
Leszek Piątek <lpiatek@gmail.com>
2017-03-14 15:05:00 +0100

    * adding `python-dotenv` to support .env file on production * updating prod.py file

--- EnviroMonitorWeb/settings/prod.py
+++ EnviroMonitorWeb/settings/prod.py
@@ -1,1 +1,18 @@
+import os
 from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = [
+    '.smogly.irynek.pl',
+    'smogly.pl',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST')
+    }
+}
