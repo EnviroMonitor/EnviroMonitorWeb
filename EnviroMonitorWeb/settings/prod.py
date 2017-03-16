79a924b23b16114e17acf64da09d74eee429de4f
Leszek Piątek <lpiatek@gmail.com>
2017-03-16 10:25:25 +0100

    changed prod settings (#58)

--- EnviroMonitorWeb/settings/prod.py
+++ EnviroMonitorWeb/settings/prod.py
@@ -3,9 +3,24 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    '.smogly.irynek.pl',
-    'smogly.pl',
+    '*',
 ]
+
+ADMINS = [
+    ('Tomasz Napierała', os.environ.get('EMAIL_ZEN')),
+    ('Leszek Piątek', os.environ.get('EMAIL_LECHUP')),
+    ('Krzysztof Hasiński', os.environ.get('EMAIL_KHASINSKI')),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
+
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30
 
 DATABASES = {
     'default': {
